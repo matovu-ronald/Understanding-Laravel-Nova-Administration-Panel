9b54d7112326bfe0e6a764fa8b47778185528b7a
Matovu Ronald <maronwhite6@gmail.com>
2019-10-16 20:38:08 +0300

    Add relationships for one to many for category and user for the posts

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $casts = [
+        'publish_at' => 'datetime',
+        'publish_until' => 'datetime',
+    ];
+
+    public function category ()
+    {
+        return $this->belongsTo('App\Category');
+    }
+
+    public function user ()
+    {
+        return $this->belongsTo('App\User');
+    }
 }
