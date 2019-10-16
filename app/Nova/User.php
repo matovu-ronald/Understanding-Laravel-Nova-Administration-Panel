0aa7b83688057ed007de7b743dcb86254c81eab1
Matovu Ronald <maronwhite6@gmail.com>
2019-10-16 20:38:26 +0300

    Add relationship HasMany field

--- app/Nova/User.php
+++ app/Nova/User.php
@@ -6,6 +6,7 @@ use Laravel\Nova\Fields\ID;
 use Illuminate\Http\Request;
 use Laravel\Nova\Fields\Text;
 use Laravel\Nova\Fields\Gravatar;
+use Laravel\Nova\Fields\HasMany;
 use Laravel\Nova\Fields\Password;
 
 class User extends Resource
@@ -60,6 +61,8 @@ class User extends Resource
                 ->onlyOnForms()
                 ->creationRules('required', 'string', 'min:8')
                 ->updateRules('nullable', 'string', 'min:8'),
+
+            HasMany::make('Posts')
         ];
     }
 
