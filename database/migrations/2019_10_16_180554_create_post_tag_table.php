1d0e684ab7f601dc52411901677a6db5a4279e97
Matovu Ronald <maronwhite6@gmail.com>
2019-10-16 21:09:37 +0300

    Create 2019_10_16_180554_create_post_tag_table.php

---
+++ database/migrations/2019_10_16_180554_create_post_tag_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostTagTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post_tag', function (Blueprint $table) {
+            $table->unsignedBigInteger('tag_id')->index();
+            $table->unsignedBigInteger('post_id')->index();
+            $table->primary(['tag_id', 'post_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post_tag');
+    }
+}
